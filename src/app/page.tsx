0bf3085cbabacff886d014432eb7746d89fc0a5d
taitapcode <hoangductai20077@gmail.com>
2025-08-02 12:46:44 +0700

    feat(home): add animated star background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { ShootingStars } from '@/components/ShootingStars';
+import { StarsBackground } from '@/components/StarBackground';
 import { Button } from '@heroui/button';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className='flex h-screen items-center justify-center'>
+      <StarsBackground />
+      <ShootingStars />
       <main className='flex flex-col items-center justify-center gap-4'>
-        <h1 className='font-fascinate text-9xl font-bold'>TaskFlow</h1>
+        <h1 className='font-fascinate text-primary text-9xl font-bold'>TaskFlow</h1>
         <h3 className='text-lg'>
           A task management app that helps you organize your tasks efficiently.
         </h3>
@@ -15,12 +19,22 @@ export default function Home() {
             color='primary'
             size='lg'
             className='w-10'
+            radius='full'
             as={Link}
             href='/signup'
+            disableRipple
           >
             Sign up
           </Button>
-          <Button color='primary' size='lg' className='w-10' as={Link} href='/login'>
+          <Button
+            color='primary'
+            size='lg'
+            className='w-10'
+            as={Link}
+            href='/login'
+            radius='full'
+            disableRipple
+          >
             Log in
           </Button>
         </div>
