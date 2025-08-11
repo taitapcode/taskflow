04ca903daeb4228c9f9952d2f51a24633ae2b3e8
taitapcode <hoangductai20077@gmail.com>
2025-08-11 17:46:43 +0700

    refactor(account): move logout logic to client component

--- src/app/app/account/page.tsx
+++ src/app/app/account/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 import { Button } from '@heroui/react';
-import { logOut } from './actions';
+import createClient from '@/lib/supabase/browser';
+import { useRouter } from 'next/navigation';
 
 export default function AccountPage() {
+  const router = useRouter();
+
+  async function handleLogOut() {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut({ scope: 'local' });
+    if (error) console.error('Error logging out:', error.message);
+    router.push('/');
+  }
+
   return (
     <>
-      <Button onPress={logOut}>Logout</Button>
+      <Button onPress={handleLogOut}>Logout</Button>
     </>
   );
 }
